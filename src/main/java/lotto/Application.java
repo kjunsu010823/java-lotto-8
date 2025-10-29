8a65a0f557dfb16eebf2e86c70527464cf74f52f
kjunsu010823 <kjunsu010823@gmail.com>
2025-10-29 13:59:57 +0900

    feat(Application): 2. Application 흐름 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import lotto.domain.*;
+import lotto.view.*;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        // 1. 입력
+        int purchaseAmount = inputView.inputPurchaseAmount();
+
+        // 2. 로또 생성
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> purchasedLottos = lottoMachine.generateLottos(purchaseAmount);
+
+        // 3. 구매한 로또 출력
+        outputView.printPurchasedLottos(purchasedLottos);
+
+        // 4. 당첨 번호 입력
+        WinningLotto winningLotto = inputView.inputWinningLotto();
+
+        // 5. 결과 계산
+        LottoResult lottoResult = new LottoResult(purchasedLottos, winningLotto);
+
+        // 6. 결과 출력
+        outputView.printResult(lottoResult);
     }
 }
